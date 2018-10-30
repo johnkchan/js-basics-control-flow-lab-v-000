30f6e69588b0b490ff9ecab0ed6d0fa2725f4470
Jonathan Chan <johnkchan252@gmail.com>
2018-10-30 02:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,11 @@ function ternaryCheckCity(city){
 }
 
 function switchOnCharmFromTip(tip){
-  
+  if (tip === 'generous'){
+    return 'Than you so much.';
+  } else if (tip === 'not as generous'){
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
